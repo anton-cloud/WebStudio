79b03f858f184fa8a6c1f543d1f7628283245acb
Yevhenii Bezeha <bezegajenja0305@gmail.com>
2021-07-20 16:53:22 +0300

    Add fixes

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileBtnClose = document.querySelector("[data-menu-close]");
+
+  menuBtnRef.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+    document.body.classList.toggle("hidden");
+  });
+
+  mobileBtnClose.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+    document.body.classList.toggle("hidden");
+  });
+})();
